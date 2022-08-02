1fc98890fa0369a55421956cd3214ba2c62e5a08
deVamshi <dev.vamshidhar@gmail.com>
2022-08-02 12:48:18 +0530

    stringsss

--- do.cpp
+++ do.cpp
@@ -1,38 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void calculateSpan(int price[], int n, int S[]){
-	stack st;
-	st.push(0); s[0] = 1;
-	for(int i = 0; i < n; I++){
-		while(!st.empty() && price[st.top()] < price[i]) st.pop();
-		s[i] = st.empty() ? i + 1 : i - st.top();
-		st.push(i);
+
+struct BinaryTreeNode {
+	int val;
+	BinaryTreeNode *left;
+	BinaryTreeNode *right;
+};
+
+
+BinaryTreeNode *create() {
+
+	int x;
+	cout << "Enter the vallue: ";
+	cin >> x;
+
+	BinaryTreeNode *p;
+	// read x from the user
+	if(x == -1) return NULL;
+
+	else {
+		p -> val = x;
+		p -> left = create();
+		p -> right = create();
 	}
-	
+
+	return p;
 }
+
+void preOrder(BinaryTreeNode *node){
+	if(!node) return;
+	cout << node -> val;
+	preOrder(node -> left);
+	preOrder(node -> right);
+}
+
 
 int main () {
 
-	stack<int> st;
+	// BinaryTreeNode *root = create();
 
-	vector<int> arr;
-	int n;
-	cout << "Enter no.of elements" << endl;
-	cin >> n;
+	cout << "E";
 
-	while(n--) {
-		int tmp;
-		cin >> tmp;
-		arr.push_back(tmp);
-	}
-
-
-
-
-
+	// preOrder(root);
 
 	return 0;
 }
-
-
